d1ff53b4b3a2ca3a13db938f33f1a35c1fbf2ecd
Sunimal Herath <sunimaldv@gmail.com>
2024-08-17 08:03:38 +1000

    implementing the bank and the account

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+#[derive(Debug)]
+struct Account {
+    id: u32,
+    balance: i32,
+    holder: String
+}
+
+impl Account {
+    fn new(id: u32, holder: String) -> Account {
+        Account {
+            id,
+            holder,
+            balance: 0
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Bank {
+    accounts: Vec<Account>
+}
+
+impl Bank {
+    fn new() -> Self {
+        Bank{
+            accounts: vec![]
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
